6be094f5b98ee841972447099921ffe29a050989
LEARN Academy <learnacademy@LEARNs-MacBook-Air.local>
2022-08-26 15:31:49 -0700

    week-3

--- code-challenges.test.js
+++ code-challenges.test.js
@@ -10,144 +10,150 @@
 // Reminder: The test will call your function
 // Run the file with the following command: $ yarn jest
 
+// a describe method that lists the name of the function OR naming of the particular test.
+// describe("hello", () => {
+//   // a test/it method, nested within the describe block, that in plain words, describes that the function does.
+//   it("returns a string that says hi", () => {
+//     //an expect method, nested within the test block, calling on the hello() function, followed by the .toEqual() matcher that checks the expected output of the function return.
+//     expect(hello()).toEqual("hi");
+//   });
+// });
 
 // --------------------1) Create a function and returns an array that length containing the numbers of the Fibonacci sequence.
 
 // a) Create a test with expect statements for each of the variables provided.
 // TDD
 // RED
+
+
 // 1- Write the test
 // 2- Run a test "yarn jest" to see failed test. 
 
 describe("number", () => {
-    it("returns an array that length containing the numbers of the Fibonacci sequence", () => {
-      expect(number()).toEqual("numbers of the Fibonacci sequence")
+
+    it("Create a function that takes in a number (greater than 2) and returns an array that length containing the numbers of the Fibonacci sequence.", () => {
+    const fibLength1 = 6
+     // Expected output: [1, 1, 2, 3, 5, 8]
+    const fibLength2 = 10
+    // Expected output: [1, 1, 2, 3, 5, 8, 13, 21, 34, 55]
+
+      expect(number(fibLength1)).toEqual([1, 1, 2, 3, 5, 8])
+      expect(number(fibLength2)).toEqual([1, 1, 2, 3, 5, 8, 13, 21, 34, 55])
     })  
 })
-
-//  FAIL  jest-dawit/jest.test.js
-  // ● number › returns an array that length containing the numbers of the Fibonacci sequence
-
-  // ReferenceError: number is not defined
-
-// GREEN
-// 3- Write the code 
-
-//Psudocode:
-// Write a function called fibLength1
-// Parameter is Fibonacci numbers of 6.
-// Expected out put of an array containg the following numbers [1, 1, 2, 3, 5, 8]
-
-//const fibLength1 = 6
-// Expected output: [1, 1, 2, 3, 5, 8]
-
-const fibLength1 = (6) => {
-  let myFibs = [1,1], i=1, a=1,b=0; b < 6; 
-     i=a, a=c, b++ 
-     myFibs.push(c)
-     
-} 
-console.log(fibLength1.myFibs())
-
-// Failed
-
-// FAIL  jest-dawit/jest.test.js
-// ● Test suite failed to run
-//   Your test suite must contain at least one test.
-
-
-
-
-//const fibLength2 = 10
-// Expected output: [1, 1, 2, 3, 5, 8, 13, 21, 34, 55]
-
-TDD
-// RED
-// 1- Write the test
-// 2- Run a test "yarn jest" to see failed test. 
-
-describe("number", () => {
-    it("returns an array that length containing the numbers of the Fibonacci sequence", () => {
-      expect(number()).toEqual("numbers of the Fibonacci sequence")
-    })  
-})
-
-//  FAIL  jest-dawit/jest.test.js
-  // ● number › returns an array that length containing the numbers of the Fibonacci sequence
-
-  // ReferenceError: number is not defined
 
 // b) Create the function that makes the test pass.
 
-const fibLength1 = (10) => {
-  let myFibs = [1,1], i=1, a=1,b=0; b < 10; 
-     i=a, a=c, b++ 
-     myFibs.push(c)
+const number = (nums) => {
+  let myFibs = [0,1];
+    for (let i=2;
+       i <= nums; 
+       i++) {
+      myFibs.push(myFibs[i-2] + myFibs[i-1])
+    } 
+     return myFibs.slice(1)
      
 } 
-console.log(fibLength1.myFibs())
 
+// PASS  ./code-challenges.test.js
+// number
+// ✓ Create a function that takes in a number (greater than 2) and returns an array that length containing the numbers of the Fibonacci sequence. (3 ms)
 
-
-// *** couldl not go further than this on the coding challeges *** On the yarn jest run test, I kept getting
-// the jason.file not found error eventhogh i am in the right directory (repositoriy)
 
 
 
 
 // --------------------2) Create a function that takes in an array and returns a new array of only odd numbers sorted from least to greatest.
 
+// a describe method that lists the name of the function OR naming of the particular test.
+// d
+
 // a) Create a test with expect statements for each of the variables provided.
 
+describe("oddNum", () => {
+  it("takes in an array and returns a new array of only odd numbers sorted from least to greatest", () => {
+    const fullArr1 = [4, 9, 0, "7", 8, true, "hey", 7, 199, -9, false, "hola"]
+   // Expected output: [-9, 7, 9, 199]
+   const fullArr2 = ["hello", 7, 23, -823, false, 78, null, "67", 6, "number"]
+   // Expected output: [-823, 7, 23]
+
+   expect(oddNum(fullArr1)).toEqual([-9, 7, 9, 199])
+   expect(oddNum(fullArr2)).toEqual([-823, 7, 23])
+   
+
+  })
+
+})
 
 
-//const fullArr1 = [4, 9, 0, "7", 8, true, "hey", 7, 199, -9, false, "hola"]
-// Expected output: [-9, 7, 9, 199]
-
-TDD
-// RED
-// 1- Write the test
-// 2- Run a test "yarn jest" to see failed test.
-
-// GREEN
-// 3- Write the code 
-
-//Psudocode:
-// Write a function called fullArr2
-// Input are numbers and string.
-// expected outout [-9, 7, 9, 199]
-
-//const fullArr2 = ["hello", 7, 23, -823, false, 78, null, "67", 6, "number"]
-// Expected output: [-823, 7, 23]
-
+// FAIL  ./code-challenges.test.js
+// number
+//   ✕ Create a function that takes in a number (greater than 2) and returns an array that length containing the numbers of the Fibonacci sequence. (1 ms)
+// oddNum
+//   ✕ takes in an array and returns a new array of only odd numbers sorted from least to greatest (3 ms)77
 
 // b) Create the function that makes the test pass.
 
+const oddNum = (array) => {
+  let newArr =[]
+  for(i = 0; i < array.length; i++){
+      if(array[i] % 2 !== 0 && typeof array[i] === 'number')
+      newArr.push(array[i])
+  }
+  return newArr.sort((a,b) => a-b)
+}
+
+// PASS  ./code-challenges.test.js
+// number
+//   ✓ Create a function that takes in a number (greater than 2) and returns an array that length containing the numbers of the Fibonacci sequence. (3 ms)
+// oddNum
+//   ✓ takes in an array and returns a new array of only odd numbers sorted from least to greatest (1 ms)
+
+
+// ****** still working on it. 
 
 // --------------------3) Create a function that takes in an array and returns an array of the accumulating sum. An empty array should return an empty array.
 
+// describe("hello", () => {
+//     // a test/it method, nested within the describe block, that in plain words, describes that the function does.
+//     it("returns a string that says hi", () => {
+//       //an expect method, nested within the test block, calling on the hello() function, followed by the .toEqual() matcher that checks the expected output of the function return.
+//       expect(hello()).toEqual("hi");
+//     });
+//   });
+
 // a) Create a test with expect statements for each of the variables provided.
 
-//const numbersToAdd1 = [2, 4, 45, 9]
-// Expected output: [2, 6, 51, 60]
 
-/ RED
-// 1- Write the test
-// 2- Run a test "yarn jest" to see failed test.
+// describe("sumArray", () => {
+//   it("takes in an array and returns an array of the accumulating sum. An empty array should return an empty array.", () => {
+const numbersToAdd1 = [2, 4, 45, 9]
+//  Expected output: [2, 6, 51, 60]
+const numbersToAdd2 = [0, 7, -8, 12]
+// // Expected output: [0, 7, -1, 11]
+const numbersToAdd3 = []
+// // Expected output: []
 
-// GREEN
-// 3- Write the code 
+// expect(sumArray(numbersToAdd1)).toEqual([2, 6, 51, 60])
+// expect(sumArray(numbersToAdd2)).toEqual([0, 7, -1, 11])
+// expect(sumArray(numbersToAdd3)).toEqual([])
 
-//Psudocode:
-// Write a function called numbersToAdd2
-// Input are numbers.
-// expected outout [0, 7, -1, 11]
+//   })
+// }) 
 
-//const numbersToAdd2 = [0, 7, -8, 12]
-// Expected output: [0, 7, -1, 11]
-
-//const numbersToAdd3 = []
-// Expected output: []
-
+// FAIL  ./code-challenges.test.js
+// number
+//   ✕ Create a function that takes in a number (greater than 2) and returns an array that length containing the numbers of the Fibonacci sequence. (1 ms)
+// oddNum
+//   ✕ takes in an array and returns a new array of only odd numbers sorted from least to greatest
+// sumArray
+//   ✕ takes in an array and returns an array of the accumulating sum. An empty array should return an empty array. (1 ms)
 
 // b) Create the function that makes the test pass.
+
+const sumArray = (array) => {
+  let newArr =[(numberstoAdd1 + numberstoAdd2 + numberstoAdd3)];
+  return newArr.sort((a,b) => a-b)
+}
+
+
